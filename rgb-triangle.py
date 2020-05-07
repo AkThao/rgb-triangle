7a9ef60ac2785372cc84aaaf66abea1cab7d8c0e
Akaash Thao <48995728+AkThao@users.noreply.github.com>
2020-05-07 22:14:18 +0100

    Add files via upload

---
+++ rgb-triangle.py
@@ -0,0 +1,36 @@
+from random import choice, randint
+
+side_len = randint(5, 10)
+char_set = ['R', 'G', 'B']
+bottom_row = [choice(char_set) for i in range(side_len)]
+
+
+def find_next_char(char1, char2):
+    if char1 == char2:
+        return char1
+
+    list_of_chars = [char for char in char_set]
+    list_of_chars.remove(char1)
+    list_of_chars.remove(char2)
+
+    return list_of_chars[0]
+
+
+def print_triangle(triangle):
+    for i in range(len(triangle)):
+        print(" " * (len(triangle) - i), end="")
+        print(" ".join(triangle[i]))
+
+
+triangle = []
+current_row = bottom_row
+while(len(current_row) > 1):
+    next_row = []
+    for i in range(len(current_row) - 1):
+        next_char = find_next_char(current_row[i], current_row[i+1])
+        next_row.append(next_char)
+    current_row = next_row
+    triangle.append(current_row)
+
+triangle = triangle[::-1]
+print_triangle(triangle)
